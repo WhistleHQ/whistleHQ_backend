4d795952bd91c4a3426a54bdba4fad0616735353
chetan <chetan@0587381121.wireless.umich.net>
2018-04-14 14:36:20 -0400

    register cleaned

--- routes/register.js
+++ routes/register.js
@@ -4,50 +4,76 @@ const userData = firebase.firestore().collection('userdata');
 const scrypt = require("scrypt");
 const scryptParameters = scrypt.paramsSync(0.2);
 
-module.exports = [(req, res, next) => {
+module.exports = (req, res, next) => {
 	res.setHeader('Content-Type', 'application/json');
 
 	if(req.body.token && req.body.username && req.body.password && req.body.name) {
-		//check for duplicate usernames
-		try {
-			var token = tokenRef.doc(req.body.token);
-			token.get().then(r => {
 
-				var data = r.data();
+		let error = [];
+		let usernameCheck, tokenCheck, domain = null;
 
-	    //check if timestamp is within 2hours
-	    if(data && data.timestamp && Date.now() - data.timestamp > 0 && Date.now() - data.timestamp < 7200000) {
-	    	scrypt.kdf(req.body.password, scryptParameters, function(err, result){
-	    		var setData = userData.add({
-	    			'username': req.body.username,
-	    			'password': result,
-	    			'name': req.body.name,
-	    			'domain': data.domain
-	    		}).then(() => {
-	    			res.send({
-	    				"success": "userdata saved"
-	    			}) ;next();
-	    		});
-	    	});
-	    } else {
-	    	res.send({
-	    		"error": "token expired"
-	    	}) ;next();
-	    }
-	}, err => {
-		res.send({
-			"error": "Incorrect token"
-		}) ;next();
-	})
-		} catch (err) {
-			res.send({
-				"error": "token expired"
-			}) ;next();
+		//Check for weak passwords
+		if (req.body.password.length < 10) {
+			error.push("Your password is weak, it should have at least 10 characters.");
 		}
+
+		//Check for duplicate usernames
+		const sameUser = userData.where("username", "==", req.body.username).get()
+			.then(snapshot => {
+				if (snapshot.size > 0) {
+					usernameCheck = false;
+					error.push("This username is taken.");
+				}	else {
+					usernameCheck = true;
+				}
+			}).catch(err => {
+				usernameCheck = false;
+				res.send("DB won't work", err); next();
+			})
+
+		//Check for a valid token
+		const invalidToken = tokenRef.doc(req.body.token).get()
+			.then(r => {
+				let data = r.data();
+				if(data && data.timestamp && Date.now() - data.timestamp > 0 && Date.now() - data.timestamp < 7200000) {
+					tokenCheck = true;
+					domain = data.domain;
+				}	else {
+					tokenCheck = false;
+					error.push("Your token has expired. Try again");
+				}
+			})
+		
+		//When both the conditions are met
+		Promise.all([sameUser, invalidToken]).then(() => {
+			if(error.length > 0) {
+				res.send({ "error": error.join(" ")});
+				next();
+				return;
+			}
+			if(usernameCheck && tokenCheck) {
+				scrypt.kdf(req.body.password, scryptParameters, function(err, result){
+					var setData = userData.add({
+						'username': req.body.username,
+						'password': result,
+						'name': req.body.name,
+						'domain': domain
+					}).then(() => {
+						res.send({
+							"success": "userdata saved"
+						}) ;next();
+					});
+				});
+			}	else {
+				res.send({
+					"success": "Something, somewhere went wrong."
+				}) ;next();
+			}
+		})
 
 	} else {
 		res.send({
 			"error": "incomplete information"
 		}) ;next();
 	}
-}]
+}
